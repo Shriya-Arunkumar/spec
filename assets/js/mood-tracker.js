91645a5355284470f4f50c9ce51f74db056c1b00
comnk <vhsiao36@gmail.com>
2024-09-05 18:05:46 -0700

    basic form setup, added js code to fetch form data from html

---
+++ assets/js/mood-tracker.js
@@ -0,0 +1,27 @@
+function toggleDescriptionContainer() {
+    if (document.querySelectorAll('input[type="radio"]:checked').length > 0) {
+        document.getElementById("description-container").style.display = "block";
+    }
+}
+
+var radios = document.querySelectorAll('input[type="radio"]');
+
+radios.forEach((radio) => {
+    radio.addEventListener('change', toggleDescriptionContainer);
+});
+
+document.getElementById("myForm").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    var description = document.getElementById("description").value;
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    data.description = description;
+
+    try {
+        console.log(data);
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+})
